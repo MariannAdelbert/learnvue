b001cccdc3f5d7451da0c2bfa1866801e4b067d5
Mariann Adelbert <mariannadelbert@gmail.com>
2025-11-28 14:46:57 +0200

    Add v-on, v-bind and v-model examples

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script setup>
+import { ref } from 'vue';
 
+    let message = ref('Hello vue');
+
+    
 </script>
 
 <template>
-    
+    <div class="container content mt-2">
+        <button class="button is-primary" @click="message = 'Hello KTA24'">Click me!</button>
+        <input class="input" v-model="message"></input>
+        <h1>{{ message.split('').reverse().join('') }}</h1>
+    </div>
 </template>
