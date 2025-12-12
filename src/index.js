4e290e88ab2796403767f17561ac8c2b45c396a8
Mariann Adelbert <mariannadelbert@gmail.com>
2025-12-12 14:17:06 +0200

    Add XMLHttpRequest example

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import { createApp } from 'vue';
-
 import App from './App.vue';
 
-import { createMemoryHistory, createWebHashHistory, createWebHistory, createRouter } from 'vue-router'
+import { createMemoryHistory, createWebHashHistory, createWebHistory, createRouter } from 'vue-router';
 
-import ToDo from './pages/ToDo.vue'
-import Modals from './pages/Modals.vue'
+import ToDo from './pages/ToDo.vue';
+import Modals from './pages/Modals.vue';
+import ChuckNorris from './pages/ChuckNorris.vue';
 
 const routes = [
-  { path: '/', component: ToDo },
-  { path: '/modals', component: Modals },
-]
+  { path: '/', component: ToDo, name: 'ToDo' },
+  { path: '/modals', component: Modals, name: 'Modals'},
+  { path: '/chuck', component: ChuckNorris, name: 'Chuck Norris'},
+];
 
 export const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
+
 
 const app = createApp(App);
-app.use(router)
+app.use(router);
 app.mount('#app');
