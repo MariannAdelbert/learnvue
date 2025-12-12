739be4c9e329ee36e42e88b50b0796402c1614dd
Mariann Adelbert <mariannadelbert@gmail.com>
2025-12-12 13:22:03 +0200

    Add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
 <script setup>
 import { ref } from 'vue';
-import Tabs from './components/Tabs.vue';
-import ToDo from './pages/ToDo.vue';
-import Modals from './pages/Modals.vue';
+import RouterTabsVue from './components/RouterTabs.vue';
 
-let titles = ref(['ToDo', 'Modals']);
-let components = ref([
-    ToDo,
-    Modals,
-]);
-let activeTab = ref(0);
-
-function setActiveTab(i){
-    activeTab.value = i;
-}
 </script>
 <template>
-   <Tabs :active="activeTab" :items="titles" @changed="setActiveTab"></Tabs>
-   <component :is="components[activeTab]"></component>
+    <RouterTabsVue></RouterTabsVue>
+    <RouterView></RouterView>
 </template>
